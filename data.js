d3759e78a45b11f4db7048c4cb8c543569ea75ac
v0416v <ellalin0416@gmail.com>
2026-05-14 19:29:51 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,6 +1,9 @@
 const lessonData = {
-    1: {
-        title: "A Private Conversation",
+  1: {
+
+      title: "A Private Conversation",
+      textEn: "Last week I went to the theatre. I had a very good seat. The play was very interesting. I did not enjoy it. A young man and a young woman were sitting behind me. They were talking loudly. I got very angry. I could not hear the actors. I turned round. I looked at the man and the woman angrily. They did not pay any attention. In the end, I could not bear it. I turned round again. 'I can't hear a word!' I said angrily.\n\n'It's none of your business,' the young man said rudely. 'This is a private conversation!'",
+      textCn: "上星期我去看戏。我的座位很好，戏很有意思，但我却无法欣赏。一位青年男子与一位青年女子正坐在我的身后，他们大声地说着话。我非常生气，因为我听不见演员在说什么。我回过头去，生气地怒视着那一男一女。他们却毫不理会。最后，我忍无可忍，又一次回过头去，生气地说：'我一个字也听不见了！'\n\n'这不关你的事，'那男的毫不客气地说，'这是私人间的谈话！'",
         words: [
             { word: "private", phonetic: "/ˈpraɪvət/", type: "adj.", trans: "私人的" },
             { word: "conversation", phonetic: "/ˌkɒnvəˈseɪʃən/", type: "n.", trans: "谈话" },
