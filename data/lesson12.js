3ca9ca5389d5f37392ca9c142b97f8858bf6049a
v0416v <ellalin0416@gmail.com>
2026-05-15 12:52:50 +0800

    Update lesson12.js

--- data/lesson12.js
+++ data/lesson12.js
@@ -1,7 +1,7 @@
 window.currentLessonData = {
     "title": "Goodbye and Good Luck",
-    "textEn": "Our neighbor, Captain Alison, will sail from Portsmouth tomorrow. We shall meet him at the harbour early in the morning. He will sail away to the South of France.",
-    "textCn": "我们的邻居艾利森船长明天将从朴次茅斯启航。我们将一大早在港口为他送行。他将驶往法国南部。",
+ "textEn": "Our neighbour, Captain Charles Alison, will sail from Portsmouth tomorrow. We shall meet him at the harbour early in the morning. He will be in his small boat, Topsail. Topsail is a famous little boat. It has sailed across the Atlantic many times. Captain Alison will set out at eight o'clock, so we shall have plenty of time. We shall see his boat and then we shall say goodbye to him. He will be away for two months. We are very proud of him. He will take part in an important race across the Atlantic.",
+  "textCn": "我们的邻居查尔斯·艾利森船长明天将从朴次茅斯启航。我们一大早会在码头为他送行。他将乘坐他的小船“涛波赛”号。“涛波赛”号是一艘有名的小艇，它已经多次横渡大西洋。艾利森船长将在8点起航，因此我们时间充裕。我们会参观他的船，然后和他告别。他要离开两个月。我们为他深感自豪。他将参加一场横渡大西洋的重要比赛。",
     "words": [
         { "word": "captain", "phonetic": "/ˈkæptɪn/", "type": "n.", "trans": "船长" },
         { "word": "sail", "phonetic": "/seɪl/", "type": "v.", "trans": "航行" },
