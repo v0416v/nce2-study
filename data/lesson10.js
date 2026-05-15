c012b0feba056e8126ae7460c453ebaa655ec532
v0416v <ellalin0416@gmail.com>
2026-05-15 11:55:24 +0800

    Update lesson10.js

--- data/lesson10.js
+++ data/lesson10.js
@@ -1,23 +1,19 @@
 window.currentLessonData = {
-    "title": "Not For Jazz",
+    "title": "Not for Jazz",
+    "textEn": "We have an old musical instrument. It is called a clavichord. It was made in Germany in 1681. Our clavichord is kept in the living room. It has belonged to our family for a long time. The instrument was bought by my grandfather many years ago. Recently it was damaged by a visitor. She tried to play jazz on it! She struck the keys too hard and two of the strings were broken. My father was shocked. Now we are not allowed to touch it. It is being repaired by a friend of my father's.",
+    "textCn": "我们有一件古老的乐器，叫古钢琴。它是1681年德国制造的。我们的这架古钢琴一直存放在起居室里。它属于我们家已经很久了。这件乐器是我祖父许多年前买的。最近它被一位客人弄坏了。她居然想用它来演奏爵士乐！她弹键太重了，两根琴弦因此断了。我父亲大为震惊。现在，任何人都不准碰它。它正在由我父亲的一位朋友修理。",
     "words": [
         {
-            "word": "musical",
-            "phonetic": "/ˈmjuːzɪkəl/",
-            "type": "adj.",
-            "trans": "音乐的"
-        },
-        {
             "word": "instrument",
-            "phonetic": "/ˈɪnstrʊmənt/",
+            "phonetic": "/ˈɪnstrəmənt/",
             "type": "n.",
             "trans": "乐器"
         },
         {
-            "word": "recent",
-            "phonetic": "/ˈriːsənt/",
-            "type": "adj.",
-            "trans": "最近的"
+            "word": "clavichord",
+            "phonetic": "/ˈklævɪkɔːd/",
+            "type": "n.",
+            "trans": "古钢琴"
         },
         {
             "word": "damage",
@@ -26,42 +22,19 @@ window.currentLessonData = {
             "trans": "损坏"
         },
         {
-            "word": "key",
-            "phonetic": "/kiː/",
-            "type": "n.",
-            "trans": "琴键；钥匙"
-        },
-        {
             "word": "string",
             "phonetic": "/strɪŋ/",
             "type": "n.",
-            "trans": "(乐器的)弦"
-        },
-        {
-            "word": "shock",
-            "phonetic": "/ʃɒk/",
-            "type": "v.",
-            "trans": "使震惊"
-        },
-        {
-            "word": "allow",
-            "phonetic": "/əˈlaʊ/",
-            "type": "v.",
-            "trans": "允许"
-        },
-        {
-            "word": "touch",
-            "phonetic": "/tʌtʃ/",
-            "type": "v.",
-            "trans": "触摸"
+            "trans": "琴弦"
         }
     ],
     "sections": [
         {
-            "sentence": "It was damaged by a visitor.",
+            "sentence": "She struck the keys too hard and two of the strings were broken.",
             "notes": [
-                "<b>was damaged by</b>: 被动语态",
-                "<b>visitor</b>: 访客"
+                "<b>struck</b>: strike 的过去式",
+                "<b>too hard</b>: 太用力",
+                "<b>were broken</b>: 被弄断了，被动语态"
             ]
         }
     ]
