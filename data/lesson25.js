ce8e3180c198a99ab5c4b1dff39971ccfe040184
v0416v <ellalin0416@gmail.com>
2026-05-15 14:56:56 +0800

    Update lesson25.js

--- data/lesson25.js
+++ data/lesson25.js
@@ -1,5 +1,7 @@
 window.currentLessonData = {
     "title": "Do the English speak English?",
+    "textEn": "I arrived in London at last. The railway station was big, black and dark. I did not know the way to my hotel, so I asked a porter. I not only spoke English very carefully, but very clearly as well. The porter, however, could not understand me. I repeated my question several times and at last he understood. He answered me, but he spoke neither slowly nor clearly. 'I am a foreigner,' I said. Then he spoke slowly, but I could not understand him. My teacher never spoke English like that! The porter and I looked at each other and smiled. Then he said something and I understood it. 'You'll soon learn English!' he said. I wonder. In England, each person speaks a different language. The English understand each other, but I don't understand them! Do they speak English?",
+"textCn": "我终于到达了伦敦。火车站又大、又黑、又昏暗。我不知道去酒店的路，于是问了一个搬运工。我不但非常小心地说英语，而且说得很清晰。然而，搬运工却听不懂我的话。我把问题重复了好几遍，他终于听懂了。他回答了我，但他话说得既不慢也不清楚。“我是个外国人，”我说。于是他放慢了语速，可我还是听不懂。我的老师从来不像那样说英语！我和搬运工面面相觑，然后笑了。接着他说了些什么，这次我听懂了。“你很快就会学会英语的！”他说。我表示怀疑。在英国，每个人说的语言都不一样。英国人之间能听懂彼此，但我听不懂他们！他们说的是英语吗？",
     "words": [
         {
             "word": "railway",
