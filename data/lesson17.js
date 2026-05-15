b0047031327b6a1ffeb592dec5cd14c824a69ba9
v0416v <ellalin0416@gmail.com>
2026-05-15 14:06:39 +0800

    Update lesson17.js

--- data/lesson17.js
+++ data/lesson17.js
@@ -1,5 +1,8 @@
 window.currentLessonData = {
     "title": "Always young",
+    "textEn": "My aunt Jennifer is an actress. She must be at least thirty-five years old. In spite of this, she often appears on the stage as a young girl. Jennifer will have to take part in a new play soon. This time, she will be a girl of seventeen. In the play, she must appear in a bright red dress and long black stockings. Last year in another play, she had to wear short socks and a bright, orange-coloured dress. If anyone ever asked her how old she is, she always answers, 'Darling, it must be terrible to be grown up!'",
+"textCn": "我的姑姑詹妮弗是位演员。她至少三十五岁了。尽管如此，她却常在舞台上扮演小姑娘。詹妮弗很快又要参加一出新剧的演出。这一次，她将扮演一个十七岁的少女。演出时她必须穿一条鲜红色的裙子和黑色的长筒袜。去年在另一出戏里，她不得不穿短袜和一件鲜艳的橘红色连衣裙。如果有人曾经问她有多大年纪，她总是回答：“亲爱的，长大成人真是太可怕了！”"
+    ,
     "words": [
         {
             "word": "appear",
