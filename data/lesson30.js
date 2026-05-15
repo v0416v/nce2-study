44672c3dbc38da25324b580b53363ef0fc793fd7
v0416v <ellalin0416@gmail.com>
2026-05-15 18:28:42 +0800

    Update lesson30.js

--- data/lesson30.js
+++ data/lesson30.js
@@ -1,5 +1,7 @@
 window.currentLessonData = {
     "title": "Football or polo?",
+    "textEn": "The Wayle is a small river that cuts across the park near my home. I like sitting by the Wayle on fine afternoons. It was warm last Sunday, so I went and sat on the river bank as usual. Some children were playing games on the bank and there were some people rowing on the river. Suddenly, one of the children kicked a ball very hard and it went towards a passing boat. The man in the boat did not see the ball and it hit him very hard. He fell into the water. All the children ran away. I turned to look at the man in the boat. He was sitting in a most uncomfortable way. The ball had hit him so hard that he had lost his balance and fallen backwards. He was still holding the oars and he was trying to get back into the boat. I laughed. 'That's football,' I said to myself, 'not polo.'",
+"textCn": "威尔河是一条流经我家附近公园的小河。天气晴朗的下午，我喜欢坐在河边。上星期天天气暖和，所以我像往常一样去坐在河岸边。一些孩子在岸上玩耍，河面上有些人在划船。突然，其中一个孩子用力踢了一个球，球朝一艘经过的船飞去。船上的人没有看到球，球重重地打在了他身上。他掉进了水里。所有的孩子都跑了。我转过头去看那个船上的人。他正以一种极不舒服的姿势坐着。球重重地打在他身上，使他失去了平衡，向后跌进了水里。他仍然握着船桨，正试图回到船上。我笑了起来。“那是足球，”我自言自语道，“不是水球。”",
     "words": [
         {
             "word": "polo",
