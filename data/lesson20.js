3573f909f6c2d84f7b06bfb6ee993fe5ade9daab
v0416v <ellalin0416@gmail.com>
2026-05-15 14:08:47 +0800

    Update lesson20.js

--- data/lesson20.js
+++ data/lesson20.js
@@ -1,5 +1,7 @@
 window.currentLessonData = {
     "title": "One man in a boat",
+    "textEn": "Fishing is my favourite sport. I often fish for hours without catching anything. But this does not worry me. Some fishermen are unlucky. Instead of catching fish, they catch old boots and rubbish. I am even less lucky. I never catch anything — not even old boots. After having spent whole mornings on the river, I always go home with an empty bag. 'You must give up fishing!' my friends say. 'It's a waste of time.' But they don't realize one important thing. I'm not really interested in fishing. I am only interested in sitting in a boat and doing nothing at all!",
+"textCn": "钓鱼是我最喜欢的运动。我常常一钓就是几个小时，却什么也钓不到。但这并不让我担心。有些钓鱼者运气不好，他们钓上来的是旧靴子和垃圾，而不是鱼。我甚至更不走运，我什么也钓不到——连旧靴子也钓不到。在河边待了整个上午之后，我总是空着袋子回家。“你必须放弃钓鱼！”我的朋友们说，“这是浪费时间。”但他们没有意识到一个重要的事实：我并不是真的对钓鱼感兴趣，我感兴趣的只是坐在船上什么也不做！",
     "words": [
         {
             "word": "fishing",
