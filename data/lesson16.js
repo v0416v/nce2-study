6cfe8855850c80e24176f8e34ced16802da5cf02
v0416v <ellalin0416@gmail.com>
2026-05-15 14:06:14 +0800

    Update lesson16.js

--- data/lesson16.js
+++ data/lesson16.js
@@ -1,5 +1,8 @@
 window.currentLessonData = {
     "title": "A polite request",
+    "textEn": "If you park your car in the wrong place, a traffic policeman will soon find it. You will be very lucky if he lets you go without a ticket. However, this does not always happen. Traffic police are sometimes very polite. During a holiday in Sweden, I found this note on my car: 'Sir, we welcome you to our city. This is a 'No Parking' area. You will enjoy your stay here if you pay attention to our street signs. This note is only a reminder.' If you receive a request like this, you cannot fail to obey it!",
+"textCn": "如果你把车停错了地方，交通警察很快就会发现。如果他没给你开罚单就让你走了，那你算很幸运。然而，这种情况并不总是发生。交通警察有时也很有礼貌。在瑞典度假期间，我在我的车上发现了这样一张纸条：“先生，欢迎您光临我们的城市。这里是‘禁止停车’区。如果您注意我们的路标，您会在这里过得愉快。这张纸条只是个提醒。”如果你收到这样的请求，你是不会不服从的！",
+    
     "words": [
         {
             "word": "park",
