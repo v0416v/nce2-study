50876e82384da13458b30a18dcff8797e4e5c4b3
v0416v <ellalin0416@gmail.com>
2026-05-15 12:53:58 +0800

    Update lesson13.js

--- data/lesson13.js
+++ data/lesson13.js
@@ -1,7 +1,7 @@
 window.currentLessonData = {
     "title": "The Greenwood Boys",
-    "textEn": "The Greenwood Boys are a group of pop singers. At present, they are visiting all parts of the country. They will arrive in London tomorrow. They will give five performances in London. Fans will be waiting for them at the station.",
-    "textCn": "格林伍德男孩是一支流行歌手组合。目前他们正在全国各地巡回演出。他们明天将到达伦敦，并将在伦敦举行五场演出。歌迷们将在车站等候他们。",
+"textEn": "The Greenwood Boys are a group of pop singers. At present, they are visiting all parts of the country. They will be arriving here tomorrow. They will be coming by train and most of the young people in the town will be meeting them at the station. Tomorrow evening they will be singing at the Workers' Club. The Greenwood Boys will be staying for five days. During this time, they will give five performances. As usual, the police will have a difficult time. They will be trying to keep order. It is always the same on these occasions.",
+"textCn": "绿林男孩是一支流行歌手乐队。目前，他们正在全国各地巡回演出。他们明天将到达这里。他们将乘火车来，镇上的大多数年轻人会在车站迎接他们。明天晚上，他们将在工人俱乐部演唱。绿林男孩将逗留五天。在此期间，他们将演出五场。像往常一样，警察将度过一段艰难的时光。他们将设法维持秩序。每逢这种场合，情况总是这样。",
     "words": [
         {
             "word": "group",
