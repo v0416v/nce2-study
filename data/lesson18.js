916707a070c916f5fefb11bc7e80675d91e37fdb
v0416v <ellalin0416@gmail.com>
2026-05-15 14:07:15 +0800

    Update lesson18.js

--- data/lesson18.js
+++ data/lesson18.js
@@ -1,5 +1,7 @@
 window.currentLessonData = {
     "title": "He often does this!",
+    "textEn": "After I had had lunch at a village pub, I looked for my bag. I had left it on a chair beside the door and now it wasn't there! As I was looking for it, the landlord came in. 'Did you have a good meal?' he asked. 'Yes, thank you,' I answered, 'but I can't pay the bill. I haven't got my bag.' The landlord smiled and immediately went out. In a few minutes he returned with my bag and gave it back to me. 'I'm very sorry,' he said. 'My dog had taken it into the garden. He often does this!'",
+"textCn": "我在一家乡村小酒馆吃完午饭后，就找我的包。我把它放在门边的椅子上了，可现在不见了！我正在寻找时，店主走了进来。“您吃得好吗？”他问。“很好，谢谢，”我回答，“但我付不了账。我的包不见了。”店主笑了笑，立刻走了出去。几分钟后，他拿着我的包回来了，把它还给了我。“非常抱歉，”他说，“我的狗把它叼到花园里去了。它经常干这种事！”",
     "words": [
         {
             "word": "pub",
