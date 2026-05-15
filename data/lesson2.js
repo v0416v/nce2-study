20173d0c91004859f39d65f69d5905bfcfc3c7f5
v0416v <ellalin0416@gmail.com>
2026-05-15 11:16:37 +0800

    Update lesson2.js

--- data/lesson2.js
+++ data/lesson2.js
@@ -1,43 +1,84 @@
 window.currentLessonData = {
-    "title": "Breakfast or lunch?",
+    "title": "Breakfast or Lunch?",
+    "textEn": "It was Sunday. I never get up early on Sundays. I sometimes stay in bed until lunchtime. Last Sunday I got up very late. I looked out of the window. It was dark outside. 'What a day!' I thought. 'It's raining again.'\n\nJust then, the telephone rang. It was my aunt Lucy. 'I've just arrived by train,' she said. 'I'm coming to see you.'\n\n'But I'm still having breakfast,' I said.\n\n'What are you doing?' she asked.\n\n'I'm having breakfast,' I repeated.\n\n'Dear me,' she said. 'Do you always get up so late? It's one o'clock!'",
+    "textCn": "那是个星期天，而我在星期天是从来不早起的。有时我要一直躺到吃午饭的时候。上个星期天，我起得很晚。我望望窗外，外面一片昏暗。‘鬼天气！’我想，‘又下雨了。’\n\n正在这时，电话铃响了。是我姑妈露西打来的。‘我刚下火车，’她说，‘我这就来看你。’\n\n‘但我还在吃早饭，’我说。\n\n‘你在干什么？’她问道。\n\n‘我正在吃早饭，’我又说了一遍。\n\n‘天啊，’她说，‘你总是起得这么晚吗？现在已经1点钟了！’",
     "words": [
         {
             "word": "until",
             "phonetic": "/ənˈtɪl/",
-            "type": "prep./conj.",
+            "type": "prep.",
             "trans": "直到"
         },
         {
             "word": "outside",
             "phonetic": "/ˌaʊtˈsaɪd/",
             "type": "adv.",
-            "trans": "外面；n. 外面"
+            "trans": "外面"
         },
         {
             "word": "ring",
             "phonetic": "/rɪŋ/",
             "type": "v.",
-            "trans": "(铃/电话)响"
+            "trans": "（铃、电话等）响"
         },
         {
             "word": "aunt",
             "phonetic": "/ɑːnt/",
             "type": "n.",
-            "trans": "姑，姨，婶"
+            "trans": "姑，姨，婶，舅妈"
         },
         {
             "word": "repeat",
             "phonetic": "/rɪˈpiːt/",
             "type": "v.",
             "trans": "重复"
+        },
+        {
+            "word": "dark",
+            "phonetic": "/dɑːk/",
+            "type": "adj.",
+            "trans": "黑暗的"
+        },
+        {
+            "word": "arrive",
+            "phonetic": "/əˈraɪv/",
+            "type": "v.",
+            "trans": "到达"
+        },
+        {
+            "word": "breakfast",
+            "phonetic": "/ˈbrekfəst/",
+            "type": "n.",
+            "trans": "早餐"
+        },
+        {
+            "word": "lunchtime",
+            "phonetic": "/ˈlʌntʃtaɪm/",
+            "type": "n.",
+            "trans": "午饭时间"
+        },
+        {
+            "word": "Sunday",
+            "phonetic": "/ˈsʌndeɪ/",
+            "type": "n.",
+            "trans": "星期日"
         }
     ],
     "sections": [
         {
-            "sentence": "It was one o'clock, but I was still in bed.",
+            "sentence": "I sometimes stay in bed until lunchtime.",
             "notes": [
-                "<b>still</b>: 仍然",
-                "<b>in bed</b>: 卧床（注意不加 the）"
+                "<b>stay in bed</b>: 待在床上",
+                "<b>until lunchtime</b>: 一直到午饭时间",
+                "<b>sometimes</b>: 频率副词，表示“有时”"
+            ]
+        },
+        {
+            "sentence": "'I'm still having breakfast,' I said.",
+            "notes": [
+                "<b>am having breakfast</b>: 现在进行时，表示正在吃早餐",
+                "<b>still</b>: 仍然，还",
+                "这里体现了说话时动作正在进行"
             ]
         }
     ]
