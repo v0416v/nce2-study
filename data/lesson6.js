d99d702e7334d0b4f48b6807a1db96871c79e298
v0416v <ellalin0416@gmail.com>
2026-05-15 11:54:15 +0800

    Update lesson6.js

--- data/lesson6.js
+++ data/lesson6.js
@@ -1,9 +1,11 @@
 window.currentLessonData = {
     "title": "Percy Buttons",
+    "textEn": "I have just moved to a house in Bridge Street. Yesterday a beggar knocked at my door. He asked me for a meal and a glass of beer. In return for this, the beggar stood on his head and sang songs. I gave him a meal. He ate the food and drank the beer. Then he put a piece of cheese in his pocket and went away. Later a neighbour told me about him. Everybody knows him. His name is Percy Buttons. He calls at every house in the street once a month and always asks for a meal and a glass of beer.",
+    "textCn": "我刚刚搬进了大桥街的一所房子。昨天，一个乞丐来敲我的门，向我讨一顿饭和一杯啤酒作为施舍。作为回报，那乞丐头顶地倒立起来，嘴里还唱着歌。我给了他一顿饭。他把食物吃完，又喝了酒。然后把一块乳酪放进衣袋里走了。后来，一位邻居告诉了我关于他的事情。大家都认识他。他叫珀西·巴顿斯。他每月一次光顾街上的每一幢房子，总是要求得到一顿饭和一杯啤酒。",
     "words": [
         {
             "word": "beggar",
-            "phonetic": "/ˈbeɡər/",
+            "phonetic": "/ˈbeɡə/",
             "type": "n.",
             "trans": "乞丐"
         },
@@ -17,7 +19,7 @@ window.currentLessonData = {
             "word": "pocket",
             "phonetic": "/ˈpɒkɪt/",
             "type": "n.",
-            "trans": "衣服口袋"
+            "trans": "衣袋"
         },
         {
             "word": "call",
@@ -28,10 +30,11 @@ window.currentLessonData = {
     ],
     "sections": [
         {
-            "sentence": "He has moved from one house to another, knocking at every door.",
+            "sentence": "In return for this, the beggar stood on his head and sang songs.",
             "notes": [
-                "<b>from...to...</b>: 从一个到另一个",
-                "<b>knocking</b>: 伴随动作，敲门"
+                "<b>in return for</b>: 作为对……的回报",
+                "<b>stood on his head</b>: 倒立",
+                "and 连接两个并列动作"
             ]
         }
     ]
