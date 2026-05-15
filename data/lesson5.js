4a15f137db199ed18febc814da917ffe1e9117d3
v0416v <ellalin0416@gmail.com>
2026-05-15 11:19:46 +0800

    Update lesson5.js

--- data/lesson5.js
+++ data/lesson5.js
@@ -1,6 +1,14 @@
 window.currentLessonData = {
     "title": "No Wrong Numbers",
+    "textEn": "Mr. James Scott has a garage in Silbury and now he has just bought another garage in Pinhurst. Pinhurst is only five miles from Silbury, but Mr. Scott cannot get a telephone for his new garage, so he has just bought twelve pigeons. Yesterday, a pigeon carried the first message from Pinhurst to Silbury. The bird covered the distance in three minutes. Up to now, Mr. Scott has sent a great many requests for spare parts and other urgent messages from one garage to the other. In this way, he has begun his own private 'telephone' service.",
+    "textCn": "詹姆斯·斯科特先生在西尔伯里有一个汽车修理部，现在他刚在平赫斯特又买了一个。平赫斯特离西尔伯里只有5英里，但斯科特先生无法为他的新修理部装电话，所以他刚买了12只鸽子。昨天，一只鸽子把第一封信从平赫斯特带到了西尔伯里。这只鸟只用了3分钟便飞完了全程。到目前为止，斯科特先生已经用这种办法发送了大量的备件请求和其他紧急信息。这样，他开始了自己的私人“电话”业务。",
     "words": [
+        {
+            "word": "garage",
+            "phonetic": "/ˈɡærɑːʒ/",
+            "type": "n.",
+            "trans": "汽车修理厂，车库"
+        },
         {
             "word": "pigeon",
             "phonetic": "/ˈpɪdʒɪn/",
@@ -17,7 +25,7 @@ window.currentLessonData = {
             "word": "cover",
             "phonetic": "/ˈkʌvə/",
             "type": "v.",
-            "trans": "越过(距离)"
+            "trans": "越过"
         },
         {
             "word": "distance",
@@ -35,21 +43,42 @@ window.currentLessonData = {
             "word": "spare",
             "phonetic": "/speə/",
             "type": "adj.",
-            "trans": "多余的"
+            "trans": "备用的"
         },
         {
             "word": "service",
             "phonetic": "/ˈsɜːvɪs/",
             "type": "n.",
             "trans": "业务，服务"
+        },
+        {
+            "word": "urgent",
+            "phonetic": "/ˈɜːdʒənt/",
+            "type": "adj.",
+            "trans": "紧急的"
+        },
+        {
+            "word": "private",
+            "phonetic": "/ˈpraɪvət/",
+            "type": "adj.",
+            "trans": "私人的"
         }
     ],
     "sections": [
         {
-            "sentence": "In this way, he has begun his own private telephone service.",
+            "sentence": "The bird covered the distance in three minutes.",
             "notes": [
-                "<b>in this way</b>: 通过这种方式",
-                "<b>own</b>: 自己的"
+                "<b>covered the distance</b>: 飞完这段距离",
+                "<b>in three minutes</b>: 在三分钟内",
+                "<b>cover</b> 在这里表示“走过、飞过（一段距离）”"
+            ]
+        },
+        {
+            "sentence": "Up to now, Mr. Scott has sent a great many requests for spare parts.",
+            "notes": [
+                "<b>Up to now</b>: 到目前为止",
+                "<b>has sent</b>: 现在完成时",
+                "<b>a great many</b>: 大量的"
             ]
         }
     ]
