d2ba23986ab668e898919edd47e51291df063377
v0416v <ellalin0416@gmail.com>
2026-05-15 18:26:15 +0800

    Update lesson29.js

--- data/lesson29.js
+++ data/lesson29.js
@@ -1,5 +1,7 @@
 window.currentLessonData = {
     "title": "Taxi!",
+    "textEn": "Captain Ben Fawcett has bought an unusual taxi and has begun a new service. The 'taxi' is a small Swiss aeroplane called a 'Pilatus Porter'. This wonderful plane can carry seven passengers. The most surprising thing about it, however, is that it can land anywhere: on snow, water, or even on a ploughed field. Captain Fawcett's first passenger was a doctor who flew from Birmingham to a lonely village in the Welsh mountains. Since then, Captain Fawcett has flown passengers to many unusual places. Once he landed on the roof of a block of flats and on another occasion, he landed in a deserted car park. Captain Fawcett has just refused a strange request from a businessman. The man wanted to fly to Rockall, a lonely island in the Atlantic Ocean, but Captain Fawcett did not take him because the trip was too dangerous.",
+"textCn": "本·福西特机长买了一辆不寻常的“出租车”，并开始了一项新的服务。这辆“出租车”是一架小型瑞士飞机，名叫“皮拉图斯·波特”号。这架奇妙的飞机可以搭载七名乘客。然而，最令人惊奇的是它可以在任何地方降落：雪地上、水面上，甚至耕过的田地里。福西特机长的第一位乘客是一位医生，他从伯明翰飞往威尔士山区的一个偏僻村庄。从那以后，福西特机长载着乘客去过许多不寻常的地方。有一次他降落在了一栋公寓楼的屋顶上，还有一次他降落在了一个废弃的停车场里。福西特机长刚刚拒绝了一位商人的奇怪请求。那人想飞往大西洋中的一个孤岛——罗卡尔岛，但福西特机长没有带他去，因为这次飞行太危险了。",
     "words": [
         {
             "word": "taxi",
