ca85b87acae03e789d531de7a31824ef85f74836
v0416v <ellalin0416@gmail.com>
2026-05-15 11:53:56 +0800

    Update lesson3.js

--- data/lesson3.js
+++ data/lesson3.js
@@ -1,12 +1,8 @@
 window.currentLessonData = {
-    "title": "Please send me a card",
+    "title": "Please Send Me a Card",
+    "textEn": "Postcards always spoil my holidays. Last summer, I went to Italy. I visited museums and sat in public gardens. A friendly waiter taught me a few words of Italian. Then he lent me a book. I read a few lines, but I did not understand a word. Every day I thought about postcards. My holidays passed quickly, but I did not send cards to my friends. On the last day I made a big decision. I got up early and bought thirty-seven cards. I spent the whole day in my room, but I did not write a single card!",
+    "textCn": "明信片总搅得我假日不得安宁。去年夏天，我去了意大利。我参观了博物馆，还去了公园。一位好客的服务员教了我几句意大利语，之后还借给我一本书。我读了几行，但一个字也不懂。每天我都想着明信片的事。假期过得很快，可我一张明信片也没给朋友们寄。到了最后一天，我作出了一项重大决定。我早早起了床，买来了37张明信片。我在房间里关了整整一天，然而竟连一张明信片也没写成！",
     "words": [
-        {
-            "word": "send",
-            "phonetic": "/send/",
-            "type": "v.",
-            "trans": "寄，送"
-        },
         {
             "word": "postcard",
             "phonetic": "/ˈpəʊstkɑːd/",
@@ -21,9 +17,15 @@ window.currentLessonData = {
         },
         {
             "word": "museum",
-            "phonetic": "/mjuːˈziːəm/",
+            "phonetic": "/mjuˈziːəm/",
             "type": "n.",
             "trans": "博物馆"
+        },
+        {
+            "word": "public",
+            "phonetic": "/ˈpʌblɪk/",
+            "type": "adj.",
+            "trans": "公共的"
         },
         {
             "word": "friendly",
@@ -57,17 +59,26 @@ window.currentLessonData = {
         },
         {
             "word": "single",
-            "phonetic": "/ˈsɪŋɡəl/",
+            "phonetic": "/ˈsɪŋɡl/",
             "type": "adj.",
-            "trans": "唯一的，单一的"
+            "trans": "单一的，一个也没有遗漏的"
         }
     ],
     "sections": [
         {
-            "sentence": "Postcards always spoil my holidays.",
+            "sentence": "A friendly waiter taught me a few words of Italian.",
             "notes": [
-                "<b>spoil</b>: 损坏，这里指心情被破坏",
-                "<b>always</b>: 频度副词，放在实义动词前"
+                "<b>teach somebody something</b>: 教某人某事",
+                "<b>a few words of Italian</b>: 几句意大利语",
+                "<b>friendly</b>: 友好的"
+            ]
+        },
+        {
+            "sentence": "I spent the whole day in my room, but I did not write a single card!",
+            "notes": [
+                "<b>spent the whole day</b>: 花了一整天",
+                "<b>a single card</b>: 连一张卡片都没有",
+                "这里用 <b>single</b> 强调“一张也没有写”"
             ]
         }
     ]
