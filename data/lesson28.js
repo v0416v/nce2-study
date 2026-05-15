db3952a6f12a6c4147c142ac3179dcefdcd1ceb4
v0416v <ellalin0416@gmail.com>
2026-05-15 18:18:10 +0800

    Update lesson28.js

--- data/lesson28.js
+++ data/lesson28.js
@@ -1,5 +1,7 @@
 window.currentLessonData = {
     "title": "No parking",
+    "textEn": "Jasper White is one of those rare people who believes in ancient myths. He has just bought a new house in the city, but ever since he moved in, he has had trouble with cars and their owners. When he returns home at night, he always finds that someone has parked a car outside his gate. Because of this, he has not been able to get his own car into his garage even once. Jasper has put up 'No Parking' signs outside his gate, but these have not had any effect. Now he has put an ugly stone head over his gate. It is one of the ugliest faces I have ever seen. I asked him what it was and he told me that it was Medusa, the Gorgon. Jasper hopes that she will turn cars and their owners to stone. But none of them has been turned to stone yet!",
+"textCn": "贾斯珀·怀特是那些相信古代神话的少数人之一。他刚在城里买了一栋新房子，但自从搬进去后，他就一直与汽车和车主们有矛盾。晚上回家时，他总是发现有人把车停在他家大门口外。因此，他一次也没能把自己的车开进车库。贾斯珀在大门外挂了“禁止停车”的牌子，但这些都没什么效果。现在他在大门上方放了一个丑陋的石雕头像。这是我见过的最丑陋的头像之一。我问他那是什么，他告诉我那是美杜莎，蛇发女妖。贾斯珀希望她能把这些汽车和车主们变成石头。但到目前为止，还没有一个人或一辆车被变成石头！"，
     "words": [
         {
             "word": "ancient",
