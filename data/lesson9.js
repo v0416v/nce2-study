b3c6fa255b0f5acf632d51f46f17f86b8a396300
v0416v <ellalin0416@gmail.com>
2026-05-15 11:55:05 +0800

    Update lesson9.js

--- data/lesson9.js
+++ data/lesson9.js
@@ -1,12 +1,8 @@
 window.currentLessonData = {
     "title": "A Cold Welcome",
+    "textEn": "On Wednesday evening, we went to the Town Hall. It was the last day of the year and a large crowd of people had gathered under the Town Hall clock. It would strike twelve in twenty minutes' time. Fifteen minutes passed and then, at five to twelve, the clock stopped. The big minute hand did not move. We waited and waited, but nothing happened. Suddenly someone shouted, 'It's two minutes past twelve! The clock has stopped!' I looked at my watch. It was true. The big clock refused to welcome the New Year. At that moment, everybody began to laugh and sing.",
+    "textCn": "星期三晚上，我们去了市政厅。那是一年的最后一天，一大群人聚集在市政厅的大钟下面。再过20分钟，大钟将敲响12下。15分钟过去了，而就在11点55分时，大钟停了。那根巨大的分针一动不动。我们等啊等，可什么事也没发生。突然有人喊道：‘已经12点过2分了！钟停了！’我看了看自己的手表，果然如此。那座大钟拒绝迎接新年。此时，大家突然都笑了起来，并唱起了歌。",
     "words": [
-        {
-            "word": "welcome",
-            "phonetic": "/ˈwelkəm/",
-            "type": "n.",
-            "trans": "欢迎"
-        },
         {
             "word": "crowd",
             "phonetic": "/kraʊd/",
@@ -20,35 +16,25 @@ window.currentLessonData = {
             "trans": "聚集"
         },
         {
+            "word": "minute",
+            "phonetic": "/ˈmɪnɪt/",
+            "type": "n.",
+            "trans": "分钟"
+        },
+        {
             "word": "hand",
             "phonetic": "/hænd/",
             "type": "n.",
-            "trans": "(钟表)指针"
-        },
-        {
-            "word": "shout",
-            "phonetic": "/ʃaʊt/",
-            "type": "v.",
-            "trans": "喊叫"
-        },
-        {
-            "word": "refuse",
-            "phonetic": "/rɪˈfjuːz/",
-            "type": "v.",
-            "trans": "拒绝"
-        },
-        {
-            "word": "laugh",
-            "phonetic": "/lɑːf/",
-            "type": "v.",
-            "trans": "笑"
+            "trans": "（钟表的）指针"
         }
     ],
     "sections": [
         {
-            "sentence": "On Wednesday night, we went to the Town Hall.",
+            "sentence": "The big clock refused to welcome the New Year.",
             "notes": [
-                "<b>On Wednesday night</b>: 在具体的周三晚上，介词用 on"
+                "<b>refused to</b>: 拒绝做某事",
+                "<b>welcome the New Year</b>: 迎接新年",
+                "这里用了拟人的修辞手法"
             ]
         }
     ]
