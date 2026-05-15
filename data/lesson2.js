ef6249adce4af3bdbac85cb4e745c5ce5972f1cf
v0416v <ellalin0416@gmail.com>
2026-05-15 10:58:12 +0800

    Add files via upload

---
+++ data/lesson2.js
@@ -0,0 +1,44 @@
+window.currentLessonData = {
+    "title": "Breakfast or lunch?",
+    "words": [
+        {
+            "word": "until",
+            "phonetic": "/ənˈtɪl/",
+            "type": "prep./conj.",
+            "trans": "直到"
+        },
+        {
+            "word": "outside",
+            "phonetic": "/ˌaʊtˈsaɪd/",
+            "type": "adv.",
+            "trans": "外面；n. 外面"
+        },
+        {
+            "word": "ring",
+            "phonetic": "/rɪŋ/",
+            "type": "v.",
+            "trans": "(铃/电话)响"
+        },
+        {
+            "word": "aunt",
+            "phonetic": "/ɑːnt/",
+            "type": "n.",
+            "trans": "姑，姨，婶"
+        },
+        {
+            "word": "repeat",
+            "phonetic": "/rɪˈpiːt/",
+            "type": "v.",
+            "trans": "重复"
+        }
+    ],
+    "sections": [
+        {
+            "sentence": "It was one o'clock, but I was still in bed.",
+            "notes": [
+                "<b>still</b>: 仍然",
+                "<b>in bed</b>: 卧床（注意不加 the）"
+            ]
+        }
+    ]
+};
