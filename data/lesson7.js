13ac25f99b8f4a71aaa6426f489b3a68a7399560
v0416v <ellalin0416@gmail.com>
2026-05-15 11:54:30 +0800

    Update lesson7.js

--- data/lesson7.js
+++ data/lesson7.js
@@ -1,5 +1,7 @@
 window.currentLessonData = {
     "title": "Too Late",
+    "textEn": "The plane was late and detectives were waiting at the airport all morning. They were expecting a valuable parcel of diamonds from South Africa. A few hours earlier, someone had told the police that thieves would try to steal the diamonds. When the plane arrived, some of the detectives were waiting inside the main building while others were waiting on the airfield. Two men took the parcel off the plane and carried it into the Customs House. While two detectives were keeping guard at the door, two others opened the parcel. To their surprise, the precious parcel was full of stones and sand!",
+    "textCn": "飞机误点了，侦探们在机场等了整整一上午。他们正期待从南非运来的一包贵重钻石。几小时前，有人向警方报告，说窃贼们将企图偷走这些钻石。当飞机到达时，一些侦探等候在主楼内，另一些则守候在停机坪上。两个人把包裹拿下飞机，搬进了海关。两名侦探守在门口，另外两名侦探打开了包裹。使他们吃惊的是，那珍贵的包裹里装的竟是石头和沙子！",
     "words": [
         {
             "word": "detective",
@@ -14,12 +16,6 @@ window.currentLessonData = {
             "trans": "机场"
         },
         {
-            "word": "expect",
-            "phonetic": "/ɪkˈspekt/",
-            "type": "v.",
-            "trans": "期待，等待"
-        },
-        {
             "word": "valuable",
             "phonetic": "/ˈvæljuəbl/",
             "type": "adj.",
@@ -27,7 +23,7 @@ window.currentLessonData = {
         },
         {
             "word": "parcel",
-            "phonetic": "/ˈpɑːsəl/",
+            "phonetic": "/ˈpɑːsl/",
             "type": "n.",
             "trans": "包裹"
         },
@@ -36,49 +32,15 @@ window.currentLessonData = {
             "phonetic": "/ˈdaɪəmənd/",
             "type": "n.",
             "trans": "钻石"
-        },
-        {
-            "word": "steal",
-            "phonetic": "/stiːl/",
-            "type": "v.",
-            "trans": "偷"
-        },
-        {
-            "word": "main",
-            "phonetic": "/meɪn/",
-            "type": "adj.",
-            "trans": "主要的"
-        },
-        {
-            "word": "guard",
-            "phonetic": "/ɡɑːd/",
-            "type": "n.",
-            "trans": "警戒，守卫"
-        },
-        {
-            "word": "precious",
-            "phonetic": "/ˈpreʃəs/",
-            "type": "adj.",
-            "trans": "珍贵的"
-        },
-        {
-            "word": "stone",
-            "phonetic": "/stəʊn/",
-            "type": "n.",
-            "trans": "石子"
-        },
-        {
-            "word": "sand",
-            "phonetic": "/sænd/",
-            "type": "n.",
-            "trans": "沙子"
         }
     ],
     "sections": [
         {
-            "sentence": "The detective was waiting at the airport.",
+            "sentence": "They were expecting a valuable parcel of diamonds from South Africa.",
             "notes": [
-                "<b>was waiting</b>: 过去进行时，强调当时正在等"
+                "<b>were expecting</b>: 过去进行时",
+                "<b>a parcel of diamonds</b>: 一包钻石",
+                "<b>from South Africa</b>: 来自南非"
             ]
         }
     ]
