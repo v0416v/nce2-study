b6ac07fbb3720e35bf29c6ebcbdc6452c84a83d9
v0416v <ellalin0416@gmail.com>
2026-05-15 14:35:26 +0800

    Update lesson21.js

--- data/lesson21.js
+++ data/lesson21.js
@@ -1,5 +1,7 @@
 window.currentLessonData = {
     "title": "Mad or not?",
+    "textEn": "Aeroplanes are slowly driving me mad. I live near an airport and passing planes can be heard night and day. The airport was built years ago, but for some reason it could not be used then. Last year, however, it came into use. Over a hundred people must have been driven away from their homes by the noise. I am one of the few people left. Sometimes I think this house will be knocked down by a passing plane. I have been offered a large sum of money to go away, but I am determined to stay here. Everybody says I must be mad and they are probably right.",
+"textCn": "飞机正在慢慢把我逼疯。我住在一个机场附近，日夜都能听到过往的飞机声。机场是多年前建的，但由于某种原因当时未能启用。然而，去年它投入了使用。有一百多人肯定已经被噪音赶走了。我是少数留下来的人之一。有时我觉得这房子会被一架过往的飞机撞倒。有人曾给我一大笔钱让我搬走，但我决心留在这里。大家都说我准是疯了，他们也许是对的。",
     "words": [
         {
             "word": "mad",
