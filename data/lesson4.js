32b639a215fc076acd892885722badd930c5c786
v0416v <ellalin0416@gmail.com>
2026-05-15 11:18:23 +0800

    Update lesson4.js

--- data/lesson4.js
+++ data/lesson4.js
@@ -1,17 +1,19 @@
 window.currentLessonData = {
     "title": "An Exciting Trip",
+    "textEn": "I have just received a letter from my brother, Tim. He is in Australia. He has been there for six months. Tim is an engineer. He is working for a big firm and he has already visited a great number of different places in Australia. He has just bought an Australian car and has gone to Alice Springs, a small town in the centre of Australia. He will soon visit Darwin. From there, he will fly to Perth. My brother has never been abroad before, so he is finding this trip very exciting.",
+    "textCn": "我刚刚收到弟弟蒂姆的一封信。他现在在澳大利亚，已经在那里待了6个月了。蒂姆是个工程师，在一家大公司工作，并且已经去过澳大利亚许多不同的地方。他刚买了一辆澳大利亚小汽车，去了澳大利亚中部的小镇艾利斯斯普林斯。他不久还将去达尔文。从那里，他再飞往珀斯。我弟弟以前从未出过国，因此，他觉得这次旅行非常激动人心。",
     "words": [
-        {
-            "word": "exciting",
-            "phonetic": "/ɪkˈsaɪtɪŋ/",
-            "type": "adj.",
-            "trans": "令人兴奋的"
-        },
         {
             "word": "receive",
             "phonetic": "/rɪˈsiːv/",
             "type": "v.",
-            "trans": "接受，收到"
+            "trans": "收到"
+        },
+        {
+            "word": "engineer",
+            "phonetic": "/ˌendʒɪˈnɪə/",
+            "type": "n.",
+            "trans": "工程师"
         },
         {
             "word": "firm",
@@ -21,13 +23,13 @@ window.currentLessonData = {
         },
         {
             "word": "different",
-            "phonetic": "/ˈdɪfərənt/",
+            "phonetic": "/ˈdɪfrənt/",
             "type": "adj.",
             "trans": "不同的"
         },
         {
             "word": "centre",
-            "phonetic": "/ˈsentər/",
+            "phonetic": "/ˈsentə/",
             "type": "n.",
             "trans": "中心"
         },
@@ -36,20 +38,47 @@ window.currentLessonData = {
             "phonetic": "/əˈbrɔːd/",
             "type": "adv.",
             "trans": "在国外"
+        },
+        {
+            "word": "already",
+            "phonetic": "/ɔːlˈredi/",
+            "type": "adv.",
+            "trans": "已经"
+        },
+        {
+            "word": "visit",
+            "phonetic": "/ˈvɪzɪt/",
+            "type": "v.",
+            "trans": "参观，访问"
+        },
+        {
+            "word": "trip",
+            "phonetic": "/trɪp/",
+            "type": "n.",
+            "trans": "旅行"
+        },
+        {
+            "word": "exciting",
+            "phonetic": "/ɪkˈsaɪtɪŋ/",
+            "type": "adj.",
+            "trans": "令人兴奋的"
         }
     ],
     "sections": [
         {
-            "sentence": "I have just received a letter from my brother, Tim.",
+            "sentence": "He has been there for six months.",
             "notes": [
-                "<b>语法点：同位语</b>。Tim 是 my brother 的同位语。",
-                "<b>have just received</b>: 现在完成时"
+                "<b>has been</b>: 现在完成时，表示从过去持续到现在",
+                "<b>for six months</b>: 持续六个月",
+                "强调动作持续的时间"
             ]
         },
         {
-            "sentence": "He is in Australia. He has been there for six months.",
+            "sentence": "My brother has never been abroad before.",
             "notes": [
-                "<b>辨析</b>：has been in (在某地待了多久)。"
+                "<b>has never been abroad</b>: 从未出过国",
+                "<b>never</b>: 从不，从未",
+                "<b>before</b>: 以前"
             ]
         }
     ]
