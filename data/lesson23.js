5a8306b2c8fb2672692da70755e4b7c0ae42ace4
v0416v <ellalin0416@gmail.com>
2026-05-15 14:36:36 +0800

    Update lesson23.js

--- data/lesson23.js
+++ data/lesson23.js
@@ -1,5 +1,7 @@
 window.currentLessonData = {
     "title": "A new house",
+    "textEn": "I had a letter from my sister yesterday. She lives in Nigeria. In her letter, she said that she would come to England next year. If she comes, she will get a surprise. We are now living in a beautiful new house in the country. Work on it had begun before my sister left. The house was completed five months ago. In my letter, I told her that she could stay with us. The house has many large rooms and there is a lovely garden. It is a very modern house, so it looks strange to some people. It must be the only modern house in the district.",
+"textCn": "昨天我收到了姐姐的一封信。她住在尼日利亚。她在信中说，她明年将来英国。如果她来了，她会感到惊讶的。我们现在住在乡下一栋漂亮的新房子里。这栋房子的修建工作在我姐姐离开之前就开始了。房子是五个月前完工的。我在信中告诉她，她可以和我们住在一起。这栋房子有许多大房间，还有一个可爱的花园。它是一栋非常现代化的房子，所以在有些人看来可能有点奇怪。它一定是这个地区唯一的一栋现代化房子。",
     "words": [
         {
             "word": "complete",
