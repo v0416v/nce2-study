a3dc71092e29fabee9e65f0640168fb2456caa85
v0416v <ellalin0416@gmail.com>
2026-05-15 14:37:30 +0800

    Update lesson24.js

--- data/lesson24.js
+++ data/lesson24.js
@@ -1,5 +1,7 @@
 window.currentLessonData = {
     "title": "It could be worse",
+    "textEn": "I entered the hotel manager's office and sat down. I had just lost $50 and I felt very upset. 'I left the money in my room,' I said, 'and it's not there now.' The manager was very sympathetic, but he could do nothing. 'Everyone's losing money these days,' he said. He started to complain about this wicked world but was interrupted by a knock at the door. A girl came in and put an envelope on his desk. It contained $50. 'I found this outside this gentleman's room,' she said. 'Well,' I said to the manager, 'there is still some honesty in this world!'",
+"textCn": "我走进酒店经理的办公室，坐了下来。我刚丢了50美元，心里非常烦恼。“我把钱放在了房间里，”我说，“可现在不见了。”经理深表同情，但却无能为力。“如今大家都在丢钱，”他说。他开始抱怨这个邪恶的世界，却被一阵敲门声打断了。一个女孩走了进来，把一个信封放在他的办公桌上。里面装着50美元。“我在这位先生的房间外面捡到的，”她说。“是啊，”我对经理说，“这个世界上还是有诚实可言的！”",
     "words": [
         {
             "word": "manager",
